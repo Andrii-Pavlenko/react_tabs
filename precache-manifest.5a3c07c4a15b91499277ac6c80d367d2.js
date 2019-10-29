74721995c78dcc00b82319f38c417ed08371043a
Andrii <farikpite@gmail.com>
2019-10-29 16:29:28 +0200

    Updates

---
+++ precache-manifest.5a3c07c4a15b91499277ac6c80d367d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48b07e2599f06b99c6a126bf8958f6f",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "b9bb1b3ccb497730ac64",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "ec14eb012e5d855c520e",
+    "url": "/react_tabs/static/js/2.9fbf4d89.chunk.js"
+  },
+  {
+    "revision": "b9bb1b3ccb497730ac64",
+    "url": "/react_tabs/static/js/main.5ac9e97b.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
